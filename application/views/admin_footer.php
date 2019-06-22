07c4b2643c53456b539e0a27a8b36fb8d550ed24
Emmanuel Chambon <echambon.contact@free.fr>
2019-06-22 23:56:24 +0200

    added PWEB_VERSION constant

--- application/views/admin_footer.php
+++ application/views/admin_footer.php
@@ -1,4 +1,4 @@
-<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds &bull; pweb version X.X.X. &bull; <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds &bull; PWEB Version <strong><?php echo PWEB_VERSION; ?></strong> &bull; <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
 </body>
