d39f882945bec325afec514c543af90260e57764
Emmanuel Chambon <echambon.contact@free.fr>
2019-06-25 23:06:14 +0200

    deleted commented code #6

--- static/js/admin_toggling.js
+++ static/js/admin_toggling.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
-	// initially hide all forms of class togglingForm
-	//$(".togglingForm").hide();
-
 	$(function() {
 	  // Set effect from select menu value
 	  $(".togglingHeader").on("click", function() {
 	    $(this).toggleClass("toggling_visible");
-			//$(this).nextAll(".togglingParagraph").first().toggle("blind");
-			//$(this).nextAll(".togglingForm").first().toggle("blind");
 			$(this).nextAll(".togglingContainer").first().toggle("fade");
 	  });
 	});
