fb69dc872f01047cb4f24af4e8d66c11485fd033
Emmanuel Chambon <echambon.contact@free.fr>
2019-06-25 23:23:28 +0200

    added link to logs page in the admin menu

--- application/views/admin_menu.php
+++ application/views/admin_menu.php
@@ -3,6 +3,7 @@
     <tr>
       <td><a href="/admin/index">Home</a></td>
       <td><a href="/admin/settings">Settings</a></td>
+      <td><a href="/admin/logs">Logs</a></td>
       <td><a href="/admin/pages">Pages</a></td>
       <td><a href="/admin/publications">Publications</a></td>
       <td><a href="/admin/style">Style</a></td>
