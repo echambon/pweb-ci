ac13c5a70a8985d53acc91b5c705fe24aa55d4b2
Emmanuel Chambon <echambon.contact@free.fr>
2019-06-25 23:23:14 +0200

    added forms #6

--- application/views/admin_settings.php
+++ application/views/admin_settings.php
@@ -1,15 +1,34 @@
 <div id="body">
   <h2 id="toggling_website_header" class="togglingHeader toggling toggling_hidden">Website</h2>
   <div id="toggling_website_settings_container" class="togglingContainer toggling_hidden">
+    <p>
+      Basic website configuration:
+    </p>
     <form id="websiteSettingsForm" method=post action="/admin/website_settings_update">
   			<table>
     			<tr>
     				<td>Title:</td>
     				<td><input type="text" id="website_title" name="website_title" size="50"></td>
+            <td><i>The title appears at the top of any page and also in the navigator tab title.</i></td>
     			</tr>
     			<tr>
     				<td>Subtitle:</td>
     				<td><input type="text" id="website_subtitle" name="website_subtitle" size="50"></td>
+            <td><i>It will appear under the website title on any page.</i></td>
+    			</tr>
+          <tr>
+    				<td>Keywords:</td>
+    				<td><input type="text" id="website_keywords" name="website_keywords" size="50"></td>
+            <td><i>Keywords are visible by search engines robots.</i></td>
+    			</tr>
+          <tr>
+    				<td>Homepage:</td>
+    				<td>
+              <select id="website_homepage" name="website_homepage">
+                <option value="-1">default</option>
+              </select>
+            </td>
+            <td><i>Choose the page you wish to use as your home page. "Default" option displays <b>pweb</b> default unpersonalized home page.</i></td>
     			</tr>
   		  </table>
       <input type="submit" value="Save">
@@ -19,7 +38,20 @@
   <h2 id="toggling_logs_header" class="togglingHeader toggling toggling_hidden">Logs</h2>
   <div id="toggling_other_container" class="togglingContainer toggling_hidden">
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut volutpat quam, eu lobortis sem. Duis non tortor ac magna feugiat scelerisque at sed justo. Suspendisse potenti. Nunc in risus ut ipsum imperdiet lacinia non et est. Aenean leo magna, semper ut dui quis, dapibus facilisis sapien. Morbi at massa quis turpis convallis facilisis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer sit amet odio est. Nunc pellentesque nisl in mi viverra mattis. Ut commodo venenatis lorem, non scelerisque tellus feugiat nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed mattis, nunc eu placerat gravida, dolor urna ultrices est, ac maximus mauris est non ipsum.
+      Connections to administration panel logs configuration:
     </p>
+    <form id="logsSettingsForm" method=post action="/admin/logs_settings_update">
+			<table>
+  			<tr>
+          <td><input type="checkbox" id="website_title" name="website_title" size="50"></td>
+  				<td>Save each time a user successfully connects to the administration panel</td>
+  			</tr>
+  			<tr>
+          <td><input type="checkbox" id="website_title" name="website_title" size="50"></td>
+  				<td>Save connections attempts to the administration panel</td>
+  			</tr>
+		  </table>
+    <input type="submit" value="Save">
+		</form>
 </div>
 </div>
