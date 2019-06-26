947fd8ac259ed59dc965f227ccbc45eb50a14895
Emmanuel Chambon <echambon.contact@free.fr>
2019-06-26 20:50:02 +0200

    updated mysql db install script

--- install/pweb-ci.sql
+++ install/pweb-ci.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 26 juin 2019 à 17:19
+-- Généré le :  mer. 26 juin 2019 à 18:49
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -36,25 +36,18 @@ CREATE TABLE IF NOT EXISTS `pw_logs` (
   `ip_address` varchar(255) NOT NULL,
   `error` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `pw_logs`
 --
 
 INSERT INTO `pw_logs` (`id`, `username`, `login_date`, `ip_address`, `error`) VALUES
-(30, 'admin', '2019-06-26 19:14:40', '::1', 0),
-(29, 'admin', '2019-06-26 19:12:56', '::1', 0),
-(28, 'admin', '2019-06-26 19:10:17', '::1', 0),
-(27, 'admin', '2019-06-26 19:06:00', '::1', 0),
-(26, 'admin', '2019-06-26 19:05:33', '::1', 0),
-(25, 'admin', '2019-06-26 18:59:46', '::1', 0),
-(24, 'admin', '2019-06-26 18:50:15', '::1', 0),
-(23, 'admin', '2019-06-25 23:50:43', '::1', 0),
-(22, 'admin', '2019-06-25 23:41:55', '::1', 0),
-(21, 'admin', '2019-06-25 23:39:50', '::1', 0),
-(31, 'admin', '2019-06-26 19:18:19', '::1', 0),
-(32, 'admin', '2019-06-26 19:18:24', '::1', 0);
+(39, 'admin', '2019-06-26 20:47:33', '::1', 0),
+(38, 'admin', '2019-06-26 20:47:27', '::1', 0),
+(37, 'admin', '2019-06-26 20:45:22', '::1', 0),
+(36, 'a', '2019-06-26 20:44:52', '::1', 2),
+(35, 'admin', '2019-06-26 20:42:36', '::1', 0);
 
 -- --------------------------------------------------------
 
@@ -69,15 +62,15 @@ CREATE TABLE IF NOT EXISTS `pw_settings` (
   `website_keywords` varchar(255) NOT NULL,
   `website_homepage_id` int(11) NOT NULL,
   `log_successful_connection_activated` tinyint(1) NOT NULL,
-  `log_failed_attemps_activated` tinyint(1) NOT NULL
+  `log_failed_attempts_activated` tinyint(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `pw_settings`
 --
 
-INSERT INTO `pw_settings` (`website_title`, `website_subtitle`, `website_keywords`, `website_homepage_id`, `log_successful_connection_activated`, `log_failed_attemps_activated`) VALUES
-('John Smith', 'Researcher', 'pweb', 0, 1, 0);
+INSERT INTO `pw_settings` (`website_title`, `website_subtitle`, `website_keywords`, `website_homepage_id`, `log_successful_connection_activated`, `log_failed_attempts_activated`) VALUES
+('John Smith', 'Researcher', 'pweb', 0, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -100,7 +93,7 @@ CREATE TABLE IF NOT EXISTS `pw_users` (
 --
 
 INSERT INTO `pw_users` (`id`, `username`, `password`, `last_login`, `email`) VALUES
-(1, 'admin', '$2y$12$vZ0xFzK6rS.iTjUPj8/wy.GdFsOhmpLcEu7BlC1KJrutPrAPTxv/q', '2019-06-26 19:18:24', 'admin@domain.ext');
+(1, 'admin', '$2y$12$vZ0xFzK6rS.iTjUPj8/wy.GdFsOhmpLcEu7BlC1KJrutPrAPTxv/q', '2019-06-26 20:47:33', 'admin@domain.ext');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
