ebdc0e3c77fa2fdef72e394f08e89eaefcb48385
Emmanuel Chambon <echambon.contact@free.fr>
2019-06-27 20:01:47 +0200

    added logs page content #14

--- application/models/logs_model.php
+++ application/models/logs_model.php
@@ -1,12 +1,12 @@
 <?php
 class logs_model extends CI_Model {
   /**
-   * Public function
-   *
-   * Insert new log entry.
-   *
-   * @return	void
-   */
+  * Public function
+  *
+  * Insert new log entry.
+  *
+  * @return	void
+  */
   public function log_admin_connection($username,$log_error_id) {
     // current datetime
     $login_date = date("Y-m-d H:i:s");
@@ -19,21 +19,21 @@ class logs_model extends CI_Model {
   }
 
   /**
-   * Public function
-   *
-   * Gets user IP adress
-   *
-   * @return	string
-   */
+  * Public function
+  *
+  * Gets user IP adress
+  *
+  * @return	string
+  */
   private function get_user_ip_address() {
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-        //ip from share internet
-        $ip = $_SERVER['HTTP_CLIENT_IP'];
+      //ip from share internet
+      $ip = $_SERVER['HTTP_CLIENT_IP'];
     } elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        //ip pass from proxy
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+      //ip pass from proxy
+      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
     } else {
-        $ip = $_SERVER['REMOTE_ADDR'];
+      $ip = $_SERVER['REMOTE_ADDR'];
     }
     return $ip;
   }
--- application/views/admin_logs.php
+++ application/views/admin_logs.php
@@ -1,3 +1,46 @@
 <div id="body">
-	<p>Hello World!</p>
+	<p>
+		Navigate through connection logs:
+	</p>
+
+	<p>
+		<form id="logsForm" method=post action="/admin/logs_query">
+			<table>
+				<tr>
+					<td>SQL query: <i>SELECT * FROM pw_logs WHERE </i></td>
+					<td><input type="text" id="sql_logs_query" name="sql_logs_query" size="50"></td>
+				</tr>
+				<tr>
+					<td>Logs to display per page:</td>
+					<td>
+						<select id="select_logs_display" name="select_logs_display">
+							<option value="10">10</option>
+					    <option value="50" selected="selected">50</option>
+					    <option value="100">100</option>
+					    <option value="all">All</option>
+						</select>
+					</td>
+				</tr>
+			</table>
+		</form>
+	</p>
+
+	<p>
+		<center><table border="1px">
+			<tr>
+				<th id="log_id" 				name="log_id">ID</th>
+				<th id="log_date" 			name="log_date">Date</th>
+				<th id="log_username" 	name="log_username">Username</th>
+				<th id="log_ip" 				name="log_ip">IP</th>
+				<th id="log_error" 			name="log_error">Error</th>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		</table></center>
+	</p>
 </div>
